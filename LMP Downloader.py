d1bbb2dbe40214bf20b83cfac62f337077078bc2
Anil <anil.maharajh@outlook.ca>
2022-10-17 17:30:03 -0400

    Optimized Code

--- LMP Downloader.py
+++ LMP Downloader.py
@@ -1,10 +1,11 @@
 from pycaiso.oasis import Node
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import csv
 import time
 
-path = "CSV/"
+PATH = "CSV/"
+TIMEOUT = 6
 
 
 def readPickle(path_loc):
@@ -17,34 +18,48 @@ def readPickle(path_loc):
 
 
 def download_LMP(name, df):
-    print(name)
-    data = []
-    node = Node(name)
-    # create dataframe with LMPS from arbitrary period (30 day maximum)
-    node_lmps = node.get_lmps(datetime(2022, 9, 1), datetime(2022, 9, 30))
-    df = pd.concat([df, node_lmps])
-    print(node_lmps)
-    mean = node_lmps.groupby(["MW"]).mean()
-    print(mean)
-    data.append([name, mean])
-    time.sleep(3)
-    return df, data
+    start, end = datetime(2022, 1, 1), datetime(2022, 1, 30)
+    while start < datetime(2022, 9, 1):
+        try:
+            print(name)
+            node = Node(name)
+            # create dataframe with LMPS from arbitrary period (30 day maximum)
+            node_lmps = node.get_lmps(start, end)
+            df = pd.concat([df, node_lmps])
+            node_df = df.drop(
+                columns=["OPR_INTERVAL", "NODE_ID_XML", "NODE", "MARKET_RUN_ID", "XML_DATA_ITEM", "PNODE_RESMRID",
+                         "GRP_TYPE",
+                         "POS", "MW"])
+            lmp = node_df[node_df["LMP_TYPE"] == "LMP"]
+            lmp.to_csv("{}{}-{}-{}.csv".format(PATH, name, start.month, start.year), index=False)
+            time.sleep(TIMEOUT)
+        except Exception as e:
+            print("Exception:\n {}".format(e))
+            time.sleep(TIMEOUT)
+            return df
+        start += timedelta(days=30)
+        end += timedelta(days=30)
+    return df
 
 
 if __name__ == "__main__":
     node_df = readPickle("node_df.pkl")
     # California Nodes 2018.csv
-    with open("temp.csv", 'r') as file:
+    with open("California Nodes 2018.csv", 'r') as file:
         reader = csv.reader(file)
         temp = False
         for row in reader:
             if temp:
-                node_df, data = download_LMP(row[2], node_df)
+                node_df = download_LMP(row[2], node_df)
             else:
                 temp = True
-    node_df.to_pickle("node_df.pkl")
-    node_df.to_csv("Raw_node.csv", index=False)
-    file = open(path + "Node Prices.csv", "w+", newline="")
-    with file:
-        write = csv.writer(file)
-        write.writerows(data)
+
+    node_df = node_df.drop(
+        columns=["OPR_INTERVAL", "NODE_ID_XML", "NODE", "MARKET_RUN_ID", "XML_DATA_ITEM", "PNODE_RESMRID", "GRP_TYPE",
+                 "POS", "MW"])
+    print(node_df)
+    lmp = node_df[node_df["LMP_TYPE"] == "LMP"]
+    print(lmp)
+    lmp.to_pickle("node_df.pkl")
+    lmp.to_csv("JAN 2019 Aug 2022 Raw_node Real.csv", index=False)
+    file = open(PATH + "Node Prices.csv", "w+", newline="")
